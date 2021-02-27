124318bf18f7a4f82db6837e1c18a096a8e2b748
rampartrange <61467700+rampartrange@users.noreply.github.com>
2021-02-27 20:37:58 +0300

    temporary commit

--- build/CMakeFiles/Makefile.cmake
+++ build/CMakeFiles/Makefile.cmake
@@ -123,5 +123,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/ParserExampleWithAst.dir/DependInfo.cmake"
+  "CMakeFiles/MiniJavaCompiler.dir/DependInfo.cmake"
   )
